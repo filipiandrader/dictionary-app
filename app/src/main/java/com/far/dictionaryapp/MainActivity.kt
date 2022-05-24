f6a05835a6955d08d2664787800f19c51f154a36
Filipi Andrade Rocha <filipiandrader@gmail.com>
2022-05-24 07:40:40 -0300

    Add progress bar

--- app/src/main/java/com/far/dictionaryapp/MainActivity.kt
+++ app/src/main/java/com/far/dictionaryapp/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -68,6 +69,10 @@ class MainActivity : ComponentActivity() {
 									}
 								}
 							}
+						}
+						
+						if (getWordInfoState.isLoading) {
+							CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
 						}
 					}
 				}
